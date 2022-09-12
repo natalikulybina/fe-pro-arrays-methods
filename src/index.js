49ed4fd3a701388bc73da063624520863ba59983
Nataliia <natali_94@ukr.net>
2022-09-12 13:12:44 +0300

    git commit -m "add homework"

--- src/index.js
+++ src/index.js
@@ -4,49 +4,49 @@ import { goods } from './goods.js';
  * @param {string} brand
  * @returns {*}
  */
-const brandFilter = (brand) => goods.filter(good => good.brand === brand);;
+const brandFilter = (brand) => goods.filter((good) => good.brand === brand);;
 
 /**
  * @param {string} color
  * @returns {*}
  */
-const colorFilter = (color) => goods.filter(good => good.color === color);
+const colorFilter = (color) => goods.filter((good) => good.color === color);
 
 /**
  * @param {string} model
  * @returns {*}
  */
-const modelFilter = (model) => goods.filter(good => good.model === model);
+const modelFilter = (model) => goods.filter((good) => good.model === model);
 
 /**
  * @param {number} memory
  * @returns {*}
  */
-const memoryFilter = (memory) => goods.filter(good => good.memory === memory);
+const memoryFilter = (memory) => goods.filter((good) => good.memory === memory);
 
 /**
  * @param {number} price
  * @returns {*}
  */
-const priceFilter = (price) =>  goods.filter(good => good.price === price);
+const priceFilter = (price) =>  goods.filter((good) => good.price === price);
 
 /**
  * @param {string} country
  * @returns {*}
  */
-const countryFilter = (country) => goods.filter(good => good.country === country);
+const countryFilter = (country) => goods.filter((good) => good.country === country);
 
 /**
  * @param {string} os
  * @returns {*}
  */
-const osFilter = (os) => goods.filter(good => good.os === os);
+const osFilter = (os) => goods.filter((good) => good.os === os);
 
 /**
  * @param {number} from
  * @param {number} to
  */
-const rangeFilter = (from, to) => goods.filter(good => good.price >= from && good.price <= to);
+const rangeFilter = (from, to) => goods.filter((good) => good.price >= from && good.price <= to);
 
 const minPriceReducer = () => goods.reduce((accum, current) => Math.min(accum, current.price), Infinity);
 console.log(minPriceReducer());
